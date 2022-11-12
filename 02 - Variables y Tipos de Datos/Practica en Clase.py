26b282721c8bd6710d3af126b7d7d16c3e39397f
nahuel470 <belinaux123@gmail.com>
2022-11-11 22:57:17 -0300

    guardar

--- 02 - Variables y Tipos de Datos/Practica en Clase.py
+++ 02 - Variables y Tipos de Datos/Practica en Clase.py
@@ -1,12 +1,2 @@
-mi_variable = 12
-print(mi_variable)
-
-mi_variable2 = 'dario'
-print(mi_variable2)
-
-mi_complejo = 5 + 7j
-print(mi_complejo)
-
-
-mi_numero = input('Ingrese un valor')
-print('el valor ingresado fue', mi_numero)
+me_gusta_el_cafe= 'lo_amo'
+print(me_gusta_el_cafe)
